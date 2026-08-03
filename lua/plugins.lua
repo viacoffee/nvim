b64aaba8092bc467b2846fcf7d6e1b642012d0e5
David Knight <231013+viacoffee@users.noreply.github.com>
2026-08-03 13:04:36 -0400

    new colors

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,41 @@
 return {
-  -- Colourscheme (onedark, matches your previous look)
+  -- Colourscheme (onedark's highlight groups, repainted with my color palette)
   {
     "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("onedark").setup { style = "darker" }
+      require("onedark").setup {
+        style = "darker",
+        colors = {
+          black = "#12161c",
+          bg0 = "#1b212a",
+          bg1 = "#212831",
+          bg2 = "#2c3540",
+          bg3 = "#333d4a",
+          bg_d = "#161a20",
+          bg_blue = "#6e99c3",
+          bg_yellow = "#d9a86c",
+          fg = "#a1aab7",
+          purple = "#b887c0",
+          green = "#99bc76",
+          orange = "#d9a86c",
+          blue = "#6e99c3",
+          yellow = "#eac390",
+          cyan = "#70b6b9",
+          red = "#d77c73",
+          grey = "#79838f",
+          light_grey = "#88929f",
+          dark_cyan = "#5c9599",
+          dark_red = "#ba5e56",
+          dark_yellow = "#b39065",
+          dark_purple = "#966f9e",
+          diff_add = "#232b26",
+          diff_delete = "#2d2325",
+          diff_change = "#1f2733",
+          diff_text = "#2c3b4d",
+        },
+      }
       require("onedark").load()
     end,
   },
